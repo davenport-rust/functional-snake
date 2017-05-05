e5c876db0d62560b420477e33c83d5922e2014f1
Christopher Davenport <ChristopherDavenport@outlook.com>
2017-05-05 16:29:20 -0400

    Apply Rustfmt

--- src/main.rs
+++ src/main.rs
@@ -12,59 +12,49 @@ pub enum Direction {
     Right,
     Left,
     Up,
-    Down
+    Down,
 }
 
 #[derive(PartialEq, Eq, Clone, Debug)]
 pub struct Board {
     rows: usize,
-    cols: usize
+    cols: usize,
 }
 
 #[derive(PartialEq, Eq, Clone, Debug)]
-pub struct Position{ x: usize, y: usize }
+pub struct Position {
+    x: usize,
+    y: usize,
+}
 
 #[derive(PartialEq, Eq, Clone, Debug)]
 pub struct Snake {
-    body: Vec<Position>
+    body: Vec<Position>,
 }
 
 impl Snake {
-
     pub fn move_directions(&self, direction: Direction) -> Self {
         match direction {
-            Direction::Left => {
-
-                Snake {body: self.body.clone()}
-            }
+            Direction::Left => Snake { body: self.body.clone() },
             Direction::Right => {
-                let new_vec : Vec<Position> = self.body.clone();
+                let new_vec: Vec<Position> = self.body.clone();
                 new_vec.last().cloned();
 
-                Snake {body: new_vec}
+                Snake { body: new_vec }
             }
             Direction::Up => {
-                let new_vec : Vec<Position> = self.body.clone();
-                Snake {body: new_vec}
+                let new_vec: Vec<Position> = self.body.clone();
+                Snake { body: new_vec }
             }
             Direction::Down => {
-                let new_vec : Vec<Position> = self.body.clone();
-                Snake {body: new_vec}
+                let new_vec: Vec<Position> = self.body.clone();
+                Snake { body: new_vec }
             }
 
         }
     }
 
     fn new() -> Self {
-        Snake { body: vec!(Position{ x:0, y: 0}) }
+        Snake { body: vec![Position { x: 0, y: 0 }] }
     }
-
 }
-
-
-
-
-
-
-
-
